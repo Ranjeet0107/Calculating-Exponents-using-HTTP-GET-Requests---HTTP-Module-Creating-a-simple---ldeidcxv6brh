1eaf02b86acc4f8383b0f62ac58fed63834aff8e
Ranjeet <ranjeet.ggi@gmail.com>
2023-04-05 10:03:48 +0530

    ok

--- src/app.js
+++ src/app.js
@@ -13,10 +13,11 @@ const server = http.createServer((req, res) => {
       const value2 = obj.num2;
 
       // Write code here to calculate power of a number
-      if(value1 <= 0 && value2>0){
+      if(value1 <= 0){
         res.statusCode=404;
         res.end("Not Found");
-      }else{
+      }
+      if(value2 < 0){
         res.statusCode=200;
       }
       
